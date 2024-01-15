55f16a96f0f18920a6f4f2ae3721457adc474465
Your Name <brianmwangi10808@gmail.com>
2024-01-15 18:08:22 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// index.js
+
+// Function to generate thank you messages
+function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  
+  // Function to count down from a given number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+
+  
